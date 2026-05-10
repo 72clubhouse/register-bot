0a1618869847be8c20d5399d1ffa2b93e0a2db44
72clubhouse <72pokerclubhouse@gmail.com>
2026-05-10 20:58:35 +0700

    Update register_bot.js

--- register_bot.js
+++ register_bot.js
@@ -2,12 +2,11 @@ const axios = require('axios');
 const http = require('http');
 
 const BOT_TOKEN = process.env.REGISTER_BOT_TOKEN || '8704643171:AAG2nd5umGh6bl0S7cT6ekBz3q-FplJXCmg';
-const NOTIFY_TOKEN = process.env.TELEGRAM_BOT_TOKEN || '8688064558:AAH4VCduJ3Aiv9rNtUT6hWBPIeMokFI_6Nw';
 const NOTIFY_CHAT_ID = process.env.TELEGRAM_CHAT_ID || '-4806445324';
 const SHEET_URL = process.env.SHEET_URL || 'https://script.google.com/macros/s/AKfycbwHVv0q9LzVL2tmS6Ye56UnC_2XiGsRAxlzAJEhncTiuj3jDtT8jNQRDDfLUxl-zC_v/exec';
 
 const API = 'https://api.telegram.org/bot' + BOT_TOKEN;
-const NOTIFY_API = 'https://api.telegram.org/bot' + NOTIFY_TOKEN;
+const NOTIFY_API = 'https://api.telegram.org/bot' + BOT_TOKEN;
 
 let offset = 0;
 const state = {};
@@ -20,7 +19,7 @@ const MSG = {
   step2: '\u0e02\u0e31\u0e49\u0e19\u0e15\u0e2d\u0e19\u0e17\u0e35\u0e48 2/4 | Step 2/4\n\n\u0e01\u0e23\u0e38\u0e13\u0e32\u0e01\u0e23\u0e2d\u0e01 <b>\u0e40\u0e1a\u0e2d\u0e23\u0e4c\u0e42\u0e17\u0e23\u0e28\u0e31\u0e1e\u0e17\u0e4c</b>\nPlease enter your <b>Phone Number</b>:',
   step3: '\u0e02\u0e31\u0e49\u0e19\u0e15\u0e2d\u0e19\u0e17\u0e35\u0e48 3/4 | Step 3/4\n\n\u0e01\u0e23\u0e38\u0e13\u0e32\u0e01\u0e23\u0e2d\u0e01 <b>\u0e0a\u0e37\u0e48\u0e2d\u0e18\u0e19\u0e32\u0e04\u0e32\u0e23 \u0e41\u0e25\u0e30\u0e40\u0e25\u0e02\u0e1a\u0e31\u0e0d\u0e0a\u0e35</b>\nPlease enter your <b>Bank Name and Account Number</b>:\n\u0e15\u0e31\u0e27\u0e2d\u0e22\u0e48\u0e32\u0e07 / Example: Kasikorn 123-4-56789-0',
   step4: '\u0e02\u0e31\u0e49\u0e19\u0e15\u0e2d\u0e19\u0e17\u0e35\u0e48 4/4 | Step 4/4\n\n\u0e01\u0e23\u0e38\u0e13\u0e32\u0e01\u0e23\u0e2d\u0e01 <b>Club GG ID</b> \u0e02\u0e2d\u0e07\u0e04\u0e38\u0e13\nPlease enter your <b>Club GG ID</b>:',
-  done: '\u2705 <b>\u0e25\u0e07\u0e17\u0e30\u0e40\u0e1a\u0e35\u0e22\u0e19\u0e2a\u0e33\u0e40\u0e23\u0e47\u0e08\u0e41\u0e25\u0e49\u0e27! / Registration Complete!</b>\n\n\u0e41\u0e2d\u0e14\u0e21\u0e34\u0e19\u0e08\u0e30\u0e15\u0e34\u0e14\u0e15\u0e48\u0e2d\u0e01\u0e25\u0e31\u0e1a\u0e43\u0e19\u0e40\u0e23\u0e47\u0e27\u0e46 \u0e19\u0e35\u0e49\u0e04\u0e23\u0e31\u0e1a\nOur admin will contact you shortly.\n\n\u{1F464} @clubhouse72',
+  done: '\u2705 <b>\u0e25\u0e07\u0e17\u0e30\u0e40\u0e1a\u0e35\u0e22\u0e19\u0e2a\u0e33\u0e40\u0e23\u0e47\u0e08\u0e41\u0e25\u0e49\u0e27! / Registration Complete!</b>\n\n\u0e01\u0e23\u0e38\u0e13\u0e32\u0e41\u0e08\u0e49\u0e07\u0e41\u0e2d\u0e14\u0e21\u0e34\u0e19\u0e40\u0e1e\u0e37\u0e48\u0e2d\u0e22\u0e37\u0e19\u0e22\u0e31\u0e19\u0e01\u0e32\u0e23\u0e25\u0e07\u0e17\u0e30\u0e40\u0e1a\u0e35\u0e22\u0e19\u0e02\u0e2d\u0e07\u0e04\u0e38\u0e13\u0e04\u0e23\u0e31\u0e1a\nPlease contact admin to confirm your registration.\n\n\u{1F464} @clubhouse72',
 };
 
 async function send(chatId, text, keyboard) {
