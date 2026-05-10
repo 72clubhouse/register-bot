b6a08c6a3194ce274c08b746dd72ef16a5eb5111
72clubhouse <72pokerclubhouse@gmail.com>
2026-05-10 20:47:15 +0700

    Update register_bot.js

--- register_bot.js
+++ register_bot.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
+const http = require('http');
 
 const BOT_TOKEN = process.env.REGISTER_BOT_TOKEN || '8704643171:AAG2nd5umGh6bl0S7cT6ekBz3q-FplJXCmg';
 const NOTIFY_TOKEN = process.env.TELEGRAM_BOT_TOKEN || '8688064558:AAH4VCduJ3Aiv9rNtUT6hWBPIeMokFI_6Nw';
-const NOTIFY_CHAT_ID = process.env.TELEGRAM_CHAT_ID || '-1003705394096';
+const NOTIFY_CHAT_ID = process.env.TELEGRAM_CHAT_ID || '-4806445324';
 const SHEET_URL = process.env.SHEET_URL || 'https://script.google.com/macros/s/AKfycbwHVv0q9LzVL2tmS6Ye56UnC_2XiGsRAxlzAJEhncTiuj3jDtT8jNQRDDfLUxl-zC_v/exec';
 
 const API = 'https://api.telegram.org/bot' + BOT_TOKEN;
@@ -10,6 +11,17 @@ const NOTIFY_API = 'https://api.telegram.org/bot' + NOTIFY_TOKEN;
 
 let offset = 0;
 const state = {};
+
+const MSG = {
+  welcome: '\u{1F0CF} <b>\u0e22\u0e34\u0e19\u0e14\u0e35\u0e15\u0e49\u0e2d\u0e19\u0e23\u0e31\u0e1a\u0e2a\u0e39\u0e48 72Clubhouse!\nWelcome to 72Clubhouse!</b>\n\n\u0e01\u0e14\u0e1b\u0e38\u0e48\u0e21\u0e14\u0e49\u0e32\u0e19\u0e25\u0e48\u0e32\u0e07\u0e40\u0e1e\u0e37\u0e48\u0e2d\u0e25\u0e07\u0e17\u0e30\u0e40\u0e1a\u0e35\u0e22\u0e19\u0e44\u0e14\u0e49\u0e40\u0e25\u0e22\u0e04\u0e23\u0e31\u0e1a\nClick the button below to register:',
+  btn_register: '\u0e25\u0e07\u0e17\u0e30\u0e40\u0e1a\u0e35\u0e22\u0e19 / Register Now',
+  btn_back: '\u0e01\u0e25\u0e31\u0e1a / Back',
+  step1: '\u0e02\u0e31\u0e49\u0e19\u0e15\u0e2d\u0e19\u0e17\u0e35\u0e48 1/4 | Step 1/4\n\n\u0e01\u0e23\u0e38\u0e13\u0e32\u0e01\u0e23\u0e2d\u0e01 <b>\u0e0a\u0e37\u0e48\u0e2d-\u0e19\u0e32\u0e21\u0e2a\u0e01\u0e38\u0e25</b>\nPlease enter your <b>Full Name</b>:',
+  step2: '\u0e02\u0e31\u0e49\u0e19\u0e15\u0e2d\u0e19\u0e17\u0e35\u0e48 2/4 | Step 2/4\n\n\u0e01\u0e23\u0e38\u0e13\u0e32\u0e01\u0e23\u0e2d\u0e01 <b>\u0e40\u0e1a\u0e2d\u0e23\u0e4c\u0e42\u0e17\u0e23\u0e28\u0e31\u0e1e\u0e17\u0e4c</b>\nPlease enter your <b>Phone Number</b>:',
+  step3: '\u0e02\u0e31\u0e49\u0e19\u0e15\u0e2d\u0e19\u0e17\u0e35\u0e48 3/4 | Step 3/4\n\n\u0e01\u0e23\u0e38\u0e13\u0e32\u0e01\u0e23\u0e2d\u0e01 <b>\u0e0a\u0e37\u0e48\u0e2d\u0e18\u0e19\u0e32\u0e04\u0e32\u0e23 \u0e41\u0e25\u0e30\u0e40\u0e25\u0e02\u0e1a\u0e31\u0e0d\u0e0a\u0e35</b>\nPlease enter your <b>Bank Name and Account Number</b>:\n\u0e15\u0e31\u0e27\u0e2d\u0e22\u0e48\u0e32\u0e07 / Example: Kasikorn 123-4-56789-0',
+  step4: '\u0e02\u0e31\u0e49\u0e19\u0e15\u0e2d\u0e19\u0e17\u0e35\u0e48 4/4 | Step 4/4\n\n\u0e01\u0e23\u0e38\u0e13\u0e32\u0e01\u0e23\u0e2d\u0e01 <b>Club GG ID</b> \u0e02\u0e2d\u0e07\u0e04\u0e38\u0e13\nPlease enter your <b>Club GG ID</b>:',
+  done: '\u2705 <b>\u0e25\u0e07\u0e17\u0e30\u0e40\u0e1a\u0e35\u0e22\u0e19\u0e2a\u0e33\u0e40\u0e23\u0e47\u0e08\u0e41\u0e25\u0e49\u0e27! / Registration Complete!</b>\n\n\u0e41\u0e2d\u0e14\u0e21\u0e34\u0e19\u0e08\u0e30\u0e15\u0e34\u0e14\u0e15\u0e48\u0e2d\u0e01\u0e25\u0e31\u0e1a\u0e43\u0e19\u0e40\u0e23\u0e47\u0e27\u0e46 \u0e19\u0e35\u0e49\u0e04\u0e23\u0e31\u0e1a\nOur admin will contact you shortly.\n\n\u{1F464} @clubhouse72',
+};
 
 async function send(chatId, text, keyboard) {
   const payload = { chat_id: chatId, text: text, parse_mode: 'HTML' };
@@ -56,17 +68,17 @@ async function handleUpdate(update) {
       if (s.step === 'name') {
         s.name = text;
         s.step = 'phone';
-        await send(chatId, '\u0e02\u0e31\u0e49\u0e19\u0e15\u0e2d\u0e19\u0e17\u0e35\u0e48 2/4\n\n\u0e01\u0e23\u0e38\u0e13\u0e32\u0e01\u0e23\u0e2d\u0e01 <b>\u0e40\u0e1a\u0e2d\u0e23\u0e4c\u0e42\u0e17\u0e23\u0e28\u0e31\u0e1e\u0e17\u0e4c</b> \u0e02\u0e2d\u0e07\u0e04\u0e38\u0e13:', null);
+        await send(chatId, MSG.step2, null);
 
       } else if (s.step === 'phone') {
         s.phone = text;
         s.step = 'bank';
-        await send(chatId, '\u0e02\u0e31\u0e49\u0e19\u0e15\u0e2d\u0e19\u0e17\u0e35\u0e48 3/4\n\n\u0e01\u0e23\u0e38\u0e13\u0e32\u0e01\u0e23\u0e2d\u0e01 <b>\u0e0a\u0e37\u0e48\u0e2d\u0e18\u0e19\u0e32\u0e04\u0e32\u0e23 \u0e41\u0e25\u0e30\u0e40\u0e25\u0e02\u0e1a\u0e31\u0e0d\u0e0a\u0e35</b>:\n\u0e15\u0e31\u0e27\u0e2d\u0e22\u0e48\u0e32\u0e07: \u0e01\u0e2a\u0e34\u0e01\u0e23 123-4-56789-0', null);
+        await send(chatId, MSG.step3, null);
 
       } else if (s.step === 'bank') {
         s.bank = text;
         s.step = 'clubgg';
-        await send(chatId, '\u0e02\u0e31\u0e49\u0e19\u0e15\u0e2d\u0e19\u0e17\u0e35\u0e48 4/4\n\n\u0e01\u0e23\u0e38\u0e13\u0e32\u0e01\u0e23\u0e2d\u0e01 <b>Club GG ID</b> \u0e02\u0e2d\u0e07\u0e04\u0e38\u0e13:', null);
+        await send(chatId, MSG.step4, null);
 
       } else if (s.step === 'clubgg') {
         s.clubgg_id = text;
@@ -80,28 +92,26 @@ async function handleUpdate(update) {
         });
 
         await notifyAdmin(
-          '๐• <b>New Member!</b>\n\n' +
-          '๐‘ค Name: ' + s.name + '\n' +
-          '๐“ Phone: ' + s.phone + '\n' +
-          '๐ฆ Bank: ' + s.bank + '\n' +
-          '๐ฎ Club GG ID: ' + s.clubgg_id + '\n' +
-          '๐“ฑ Telegram: @' + (update.message.from.username || userId)
+          '\u{1F195} <b>New Member!</b>\n\n' +
+          '\u{1F464} Name: ' + s.name + '\n' +
+          '\u{1F4DE} Phone: ' + s.phone + '\n' +
+          '\u{1F3E6} Bank: ' + s.bank + '\n' +
+          '\u{1F3AE} Club GG ID: ' + s.clubgg_id + '\n' +
+          '\u{1F4F1} Telegram: @' + (update.message.from.username || userId)
         );
 
         delete state[userId];
 
-        await send(chatId,
-          '\u2705 <b>\u0e25\u0e07\u0e17\u0e30\u0e40\u0e1a\u0e35\u0e22\u0e19\u0e2a\u0e33\u0e40\u0e23\u0e47\u0e08\u0e41\u0e25\u0e49\u0e27! / Registration Complete!</b>\n\n\u0e41\u0e2d\u0e14\u0e21\u0e34\u0e19\u0e08\u0e30\u0e15\u0e34\u0e14\u0e15\u0e48\u0e2d\u0e01\u0e25\u0e31\u0e1a\u0e43\u0e19\u0e40\u0e23\u0e47\u0e27\u0e46 \u0e04\u0e23\u0e31\u0e1a\nOur admin will contact you shortly.\n\n๐‘ค @clubhouse72',
-          [[{ text: '\u0e01\u0e25\u0e31\u0e1a / Back', callback_data: 'back' }]]
+        await send(chatId, MSG.done,
+          [[{ text: MSG.btn_back, callback_data: 'back' }]]
         );
       }
       return;
     }
 
     if (text === '/start') {
-      await send(chatId,
-        '๐ <b>\u0e22\u0e34\u0e19\u0e14\u0e35\u0e15\u0e49\u0e2d\u0e19\u0e23\u0e31\u0e1a\u0e2a\u0e39\u0e48 72Clubhouse!</b>\n\n\u0e01\u0e14\u0e1b\u0e38\u0e48\u0e21\u0e14\u0e49\u0e32\u0e19\u0e25\u0e48\u0e32\u0e07\u0e40\u0e1e\u0e37\u0e48\u0e2d\u0e25\u0e07\u0e17\u0e30\u0e40\u0e1a\u0e35\u0e22\u0e19\u0e44\u0e14\u0e49\u0e40\u0e25\u0e22\u0e04\u0e23\u0e31\u0e1a:',
-        [[{ text: '\u0e25\u0e07\u0e17\u0e30\u0e40\u0e1a\u0e35\u0e22\u0e19 / Register Now', callback_data: 'register' }]]
+      await send(chatId, MSG.welcome,
+        [[{ text: MSG.btn_register, callback_data: 'register' }]]
       );
     }
   }
@@ -116,12 +126,11 @@ async function handleUpdate(update) {
 
     if (data === 'register') {
       state[userId] = { step: 'name' };
-      await send(chatId, '\u0e02\u0e31\u0e49\u0e19\u0e15\u0e2d\u0e19\u0e17\u0e35\u0e48 1/4 | Step 1/4\n\n\u0e01\u0e23\u0e38\u0e13\u0e32\u0e01\u0e23\u0e2d\u0e01 <b>\u0e0a\u0e37\u0e48\u0e2d-\u0e19\u0e32\u0e21\u0e2a\u0e01\u0e38\u0e25</b>\nPlease enter your <b>Full Name</b>:', null);
+      await send(chatId, MSG.step1, null);
 
     } else if (data === 'back') {
-      await send(chatId,
-        '๐ <b>\u0e22\u0e34\u0e19\u0e14\u0e35\u0e15\u0e49\u0e2d\u0e19\u0e23\u0e31\u0e1a\u0e2a\u0e39\u0e48 72Clubhouse!</b>\n\n\u0e01\u0e14\u0e1b\u0e38\u0e48\u0e21\u0e14\u0e49\u0e32\u0e19\u0e25\u0e48\u0e32\u0e07\u0e40\u0e1e\u0e37\u0e48\u0e2d\u0e25\u0e07\u0e17\u0e30\u0e40\u0e1a\u0e35\u0e22\u0e19:',
-        [[{ text: '\u0e25\u0e07\u0e17\u0e30\u0e40\u0e1a\u0e35\u0e22\u0e19 / Register Now', callback_data: 'register' }]]
+      await send(chatId, MSG.welcome,
+        [[{ text: MSG.btn_register, callback_data: 'register' }]]
       );
     }
   }
@@ -144,7 +153,6 @@ async function poll() {
   setTimeout(poll, 1000);
 }
 
-const http = require('http');
 const PORT = process.env.PORT || 3000;
 http.createServer(function(req, res) {
   res.writeHead(200);
