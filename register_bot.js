53202383d93a4a9b00f658aefc133eac235ddfd9
72clubhouse <72pokerclubhouse@gmail.com>
2026-05-14 00:23:05 +0700

    Update register_bot.js

--- register_bot.js
+++ register_bot.js
@@ -108,6 +108,7 @@ async function handleUpdate(update) {
         await saveSheet({
           name: s.name, phone: s.phone, bank: s.bank,
           clubgg_id: s.clubgg_id,
+          telegram_username: (update.message.from.username ? '@' + update.message.from.username : ''),
           telegram_id: String(userId),
         });
         var e1 = String.fromCodePoint(0x1F195);
@@ -122,7 +123,7 @@ async function handleUpdate(update) {
           e3 + ' Phone: ' + s.phone + '\n' +
           e4 + ' Bank: ' + s.bank + '\n' +
           e5 + ' Club GG ID: ' + s.clubgg_id + '\n' +
-          e6 + ' Telegram ID: ' + String(userId)
+          e6 + ' Telegram: ' + (update.message.from.username ? '@' + update.message.from.username : '') + ' (' + userId + ')'
         );
         s.step = 'rematch';
         await send(chatId, MSG.rematch_step, [
